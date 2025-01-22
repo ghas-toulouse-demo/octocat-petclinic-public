c66ad392cea40834a4cf271e09f3b6ebd30fd708
Tugdual Grall <tgrall@github.com>
2025-01-22 07:11:07 +0200

    Add Azure CLI script and Kubernetes deployment for petclinic application

---
+++ kubernetes/cloud/az-cli.sh
@@ -0,0 +1,108 @@
+
+az group create --name myRGNetwork --location eastus
+
+az network vnet create \
+  --resource-group myRGNetwork \
+  --name myVNet \
+  --address-prefix 10.0.0.0/16 \
+  --subnet-name myFrontendSubnet \
+  --subnet-prefix 10.0.1.0/24
+
+az network vnet subnet create \
+  --resource-group myRGNetwork \
+  --vnet-name myVNet \
+  --name myBackendSubnet \
+  --address-prefix 10.0.2.0/24
+
+az network public-ip create --resource-group myRGNetwork --name myPublicIPAddress
+
+az vm deallocate --resource-group myRGNetwork --name myFrontendVM
+
+az network public-ip update --resource-group myRGNetwork --name myPublicIPAddress --allocation-method static
+
+az vm start --resource-group myRGNetwork --name myFrontendVM --no-wait
+
+az vm create \
+  --resource-group myRGNetwork \
+  --name myFrontendVM \
+  --vnet-name myVNet \
+  --subnet myFrontendSubnet \
+  --nsg myFrontendNSG \
+  --public-ip-address myPublicIPAddress \
+  --image Ubuntu2204 \
+  --generate-ssh-keys
+
+az network nsg create --resource-group myRGNetwork --name myBackendNSG
+
+az network vnet subnet update \
+  --resource-group myRGNetwork \
+  --vnet-name myVNet \
+  --name myBackendSubnet \
+  --network-security-group myBackendNSG
+
+az network nsg rule create \
+  --resource-group myRGNetwork \
+  --nsg-name myFrontendNSG \
+  --name http \
+  --access allow \
+  --protocol Tcp \
+  --direction Inbound \
+  --priority 200 \
+  --source-address-prefix "*" \
+  --source-port-range "*" \
+  --destination-address-prefix "*" \
+  --destination-port-range 80
+
+az network nsg rule list --resource-group myRGNetwork --nsg-name myFrontendNSG --output table
+
+az network nsg rule create \
+  --resource-group myRGNetwork \
+  --nsg-name myBackendNSG \
+  --name SSH \
+  --access Allow \
+  --protocol Tcp \
+  --direction Inbound \
+  --priority 100 \
+  --source-address-prefix 10.0.1.0/24 \
+  --source-port-range "*" \
+  --destination-address-prefix "*" \
+  --destination-port-range "22"
+
+az network nsg rule create \
+  --resource-group myRGNetwork \
+  --nsg-name myBackendNSG \
+  --name MySQL \
+  --access Allow \
+  --protocol Tcp \
+  --direction Inbound \
+  --priority 200 \
+  --source-address-prefix 10.0.1.0/24 \
+  --source-port-range "*" \
+  --destination-address-prefix "*" \
+  --destination-port-range "3306"
+
+  az network nsg rule create \
+  --resource-group myRGNetwork \
+  --nsg-name myBackendNSG \
+  --name denyAll \
+  --access Deny \
+  --protocol Tcp \
+  --direction Inbound \
+  --priority 300 \
+  --source-address-prefix "*" \
+  --source-port-range "*" \
+  --destination-address-prefix "*" \
+  --destination-port-range "*"  
+
+az vm create \
+  --resource-group myRGNetwork \
+  --name myBackendVM \
+  --vnet-name myVNet \
+  --subnet myBackendSubnet \
+  --public-ip-address "" \
+  --nsg "" \
+  --image Ubuntu2204 \
+  --generate-ssh-keys
+
+az network nsg rule list --resource-group myRGNetwork --nsg-name myBackendNSG --output table
+
